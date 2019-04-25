93a9682b19b4562e7bb75a545adf9eb6c84bc906
pkuznetsov <aimanptz@yandex.ru>
2019-04-25 14:14:41 +0300

    console.log deletetd

--- src/components/card/_card.js
+++
@@ -1,6 +0,0 @@
-console.log('123123');
-
-const add = (x) => console.log(x + 5);
-
-add(3);
-
--- src/components/card/_cardCompiled.js
+++ src/components/card/_cardCompiled.js
@@ -1,10 +1,2 @@
 "use strict";
 
-console.log('123123');
-
-var add = function add(x) {
-  return console.log(x + 5);
-};
-
-add(3);
-
