f8699ec81e45ced3c38e75ad8a23a9a415474a00
pkuznetsov <aimanptz@yandex.ru>
2019-04-25 13:47:39 +0300

    ITRACKER-9239 setup to compile es6 locally

--- src/components/card/_card.js
+++ src/components/card/_card.js
@@ -1,1 +1,6 @@
-console.log('kokook');
+console.log('123123');
+
+const add = (x) => console.log(x + 5);
+
+add(3);
+
---
+++ src/components/card/_cardCompiled.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log('123123');
+
+var add = function add(x) {
+  return console.log(x + 5);
+};
+
+add(3);
+
