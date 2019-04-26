6ec549dd4cb3144613cb74002aae995646b760a3
pkuznetsov <aimanptz@yandex.ru>
2019-04-26 14:38:18 +0300

    ITRACKER-9239 master refresh
    ITRACKER-9239 action card first type ready
    
    ITRACKER-9239 flipper ready
    
    ITRACKER-9239 both action cards done
    
    ITRACKER-9239 action card dev refresh master
    
    ITRACKER-9239 action card dev refresh from katya
    
    ITRACKER-9239 cards ready for check
    
    ITRACKER-9239 cards ready for test
    
    ITRACKER-9239 cards, slider, mobile view, goal cards

---
+++ src/components/card/_card.js
@@ -0,0 +1,8 @@
+/* Card Flipping Script*/
+
+import './scripts/flipCards';
+
+/* Swiper Slider*/
+
+import './scripts/checkinSlider';
+
--- src/components/tooltip/_tooltip.js
+++ src/components/tooltip/_tooltip.js
@@ -10,9 +10,15 @@ if (window.innerWidth < TABLET) {
         for (let i = 0; i < tooltipHover.length; i++) {
             tooltipHover[i].addEventListener('click', function (e) {
                 e.preventDefault();
-                tooltip[i].classList.add('show');
-                screen.classList.add('is-visible');
                 
+                if ( this.parentElement.classList.contains('card-rating')
+                    && (window.innerWidth <= 820) ) {
+                        tooltip[i].style.display = 'none';
+                } else {
+                    tooltip[i].classList.add('show');
+                    screen.classList.add('is-visible');
+                }
+    
                 screen.addEventListener('click', function () {
                     tooltip[i].classList.remove('show');
                     this.classList.remove('is-visible');
