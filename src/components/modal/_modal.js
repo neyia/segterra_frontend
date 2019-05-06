2fb444914ad13ea9ae01160bfe1240202cae2d52
EkaterinaKaliago <kaliago@yandex.ru>
2019-05-06 19:36:59 +0300

    ITRACKER-9237

--- src/components/modal/_modal.js
+++ src/components/modal/_modal.js
@@ -1,10 +1,13 @@
-var popup = document.getElementById('modal');
-var cancelBtn = document.getElementById('modal__close');
-
-cancelBtn.onclick = function() {
-    closePopup();
-};
-
-function closePopup() {
-    popup.classList.remove('show');
+function CloseModal() {
+    var modal = document.querySelectorAll('.modal');
+    var cancelBtn = document.querySelectorAll('.modal__close');
+    
+    for (let i = 0; i < cancelBtn.length; i++) {
+        cancelBtn[i].addEventListener('click', function (e) {
+            e.preventDefault();
+            modal[i].classList.remove('show');
+        });
+    }
 }
+
+CloseModal();
