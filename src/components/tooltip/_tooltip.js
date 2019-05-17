148146a54f031a1c144ed3028c9d870eee2ed4dc
Natallia <nzhyrkova@biarum.com>
2019-05-17 18:33:14 +0300

    tooltips

--- src/components/tooltip/_tooltip.js
+++ src/components/tooltip/_tooltip.js
@@ -1,32 +1,44 @@
 const TABLET = 796;
-if (window.innerWidth < TABLET) {
-    
-    function Tooltip() {
-        
-        const tooltipHover = document.querySelectorAll('.tooltip-hover');
-        const tooltip = document.querySelectorAll('.tooltip');
-        const screen = document.querySelector('.screen');
-        
-        for (let i = 0; i < tooltipHover.length; i++) {
-            tooltipHover[i].addEventListener('click', function (e) {
+
+function Tooltip() {
+    const tooltipHover = document.querySelectorAll('.tooltip-hover');
+
+    const tooltip = document.querySelectorAll('.tooltip');
+    const screen = document.querySelector('.screen');
+
+    for (let i = 0; i < tooltipHover.length; i++) {
+        const tooltipIcon = document.querySelectorAll('.tooltip-icon');
+
+        tooltipIcon[i].addEventListener('click', function (e) {
+            e.preventDefault();
+
+            /* if (this.parentElement.classList.contains('card-rating')
+                && (window.innerWidth <= 820)) {
+                tooltip[i].style.display = 'none';
+            } else {*/
+
+            if (window.innerWidth < TABLET) {
+                tooltip[i].classList.add('show');
+                screen.classList.add('is-visible');
+            }
+
+            screen.addEventListener('click', function () {
                 e.preventDefault();
-                
-                if ( this.parentElement.classList.contains('card-rating')
-                    && (window.innerWidth <= 820) ) {
-                        tooltip[i].style.display = 'none';
-                } else {
-                    tooltip[i].classList.add('show');
-                    screen.classList.add('is-visible');
-                }
-    
-                screen.addEventListener('click', function () {
+
                     tooltip[i].classList.remove('show');
                     this.classList.remove('is-visible');
-                });
+
             });
-        }
+        });
     }
-    
+}
+
+if (window.innerWidth <= TABLET) {
     Tooltip();
-    
+}
+
+window.onresize = function () {
+    if (window.innerWidth < TABLET) {
+        Tooltip();
+    }
 }
