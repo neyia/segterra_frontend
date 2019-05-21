fdad4f12afbef4922e175f51c4faf2a4b2270ffe
Natallia <nzhyrkova@biarum.com>
2019-05-21 17:55:52 +0300

    calendar height ui fixes

--- src/components/calendar/_calendar.js
+++ src/components/calendar/_calendar.js
@@ -1,22 +1,27 @@
 window.addEventListener('load', () => {
-    
+
     // Change default button text
     $.fn.datepicker.language['en'] = {
         ...$.fn.datepicker.language['en'],
         clear: 'Cancel'
     };
-    
+
     // Create 'OK' button
     const span = document.createElement('span');
     span.innerText = 'Ok';
     span.className = 'datepicker--button';
-    
+
+
+
     // Type in your element's classname
     $('.calendar').datepicker({
         language: 'en',
         firstDay: 1,
         clearButton: true,
         onShow: (picker) => {
+            const body = document.querySelector('body');
+            body.style.height = '480px'
+            console.log('one');
             // Add 'OK' button to the datepicker
             document.querySelector('.datepicker--buttons').appendChild(span)
             // Add 'click' listener to the 'OK' button
@@ -27,6 +32,6 @@ window.addEventListener('load', () => {
                 picker.hide();
             });
         },
-        
+
     });
 });
