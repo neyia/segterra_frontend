544f47a47b90eb4c18c5910cd6f84f2d6a42ab98
Natallia <nzhyrkova@biarum.com>
2019-05-22 12:05:59 +0300

    switchers ui fixes

--- src/components/switcher/_switcher.js
+++ src/components/switcher/_switcher.js
@@ -1,6 +1,6 @@
 function Toggler() {
     const toggleItem = document.querySelectorAll('.toggler__item');
-    
+
     for (let i = 0; i < toggleItem.length; i++) {
         for (let j = 0; j < toggleItem.length; j++) {
             toggleItem[i].addEventListener('click', function () {
@@ -12,3 +12,27 @@ function Toggler() {
 }
 
 Toggler();
+
+
+let checkbox = document.querySelector('.tumbler__input');
+let label = document.querySelector('.tumbler-flex');
+
+checkbox.addEventListener('change', function(event) {
+    if(event.target.checked===true){
+        document.querySelector('.tumbler-label').textContent="On";
+    }
+    else{
+        document.querySelector('.tumbler-label').textContent="Off";
+    }
+});
+
+button.addEventListener('click', function() {
+    checkbox.checked = !checkbox.checked;
+    triggerEvent(checkbox, 'change');
+});
+
+function triggerEvent(element, eventName) {
+    let event = document.createEvent("HTMLEvents");
+    event.initEvent(eventName, false, true);
+    element.dispatchEvent(event);
+}
