0646787dc235a3556e81227625034e73b7d1c926
EkaterinaKaliago <kaliago@yandex.ru>
2019-07-01 13:47:48 +0300

    ITRACKER-9292 Feed section fixes after test

--- src/hubspot/scripts/hubspot.js
+++ src/hubspot/scripts/hubspot.js
@@ -1,5 +1,6 @@
 /** Delete ALL imports before adding to HubSpot*/
 import PopUp from './modules/PopUp';
+import './modules/feed';
 
 window.addEventListener('load', () => {
     mobileMenu();
--- src/hubspot/scripts/modules/PopUp.js
+++ src/hubspot/scripts/modules/PopUp.js
@@ -9,19 +9,21 @@ const hidePopUp = Symbol();
 const PopUp = class {
     /**
      * Create a PopUp
-     * @param  {Element|Element[]} btnShow - Dom Element or Array of Dom Elements to add 'show' class on click.
-     * @param  {Element} popup - Dom Element used as a popup to get 'show' class.
-     * @param  {Element|Element[]} [btnClose=popup] - Dom Element or Array of Dom Elements to remove 'show' class on
-     *     click.
-     * @param  {String} [className='show'] - Class name to be added on click.
-     * @param  {String} [actionShow=click] - Event to add 'show' class on click.
-     * @param  {String} [actionHide=click] - Event to remove 'show' class on click.
-     * @returns {Object} - PopUp instance with button(s) to toggle 'show' class.
+     @param  {Element|Element[]} btnShow - Dom Element or Array of Dom Elements to add 'show' class on click.
+     @param  {Element} popup - Dom Element used as a popup to get 'show' class.
+     @param  {Element|Element[]} [btnClose=popup] - Dom Element or Array of Dom Elements to remove 'show' class on
+     click.
+     @param  {Boolean} [noScroll=false] - Prevent scroll on window.
+     @param  {String} [className='show'] - Class name to be added on click.
+     @param  {String} [actionShow=click] - Event to add 'show' class on click.
+     @param  {String} [actionHide=click] - Event to remove 'show' class on click.
+     @returns {Object} - PopUp instance with button(s) to toggle 'show' class.
      */
-    constructor(btnShow, popup, btnClose, className, actionShow, actionHide) {
+    constructor(btnShow, popup, btnClose, noScroll, className, actionShow, actionHide) {
         this.btnShow = btnShow;
         this.popup = popup;
         this.btnClose = btnClose || popup;
+        this.noScroll = noScroll || false;
         this.className = className || 'show';
         this.actionShow = actionShow || 'click';
         this.actionHide = actionHide || 'click';
@@ -29,23 +31,44 @@ const PopUp = class {
         /** Reveal PopUp(add 'show' class), activate Close Button(s) & prevent body scroll*/
         if (!Array.isArray(this.btnShow)) {
             this.btnShow.addEventListener(this.actionShow, () => {
-                this.popup.classList.add(this.className);
-                this[addEventListenersToCloseBtn]();
+                if (this.btnShow === this.btnClose) {
+                    this.popup.classList.toggle(this.className);
+                } else {
+                    this.addClasslist();
+                }
                 
-                document.body.classList.add('no-scroll');
-                window.addEventListener("touchmove", PopUp.preventScrollOnMobile);
+                if (this.noScroll) {
+                    PopUp.addNoScroll();
+                }
             });
         } else {
             this.btnShow.forEach(btn => {
                 btn.addEventListener(this.actionShow, () => {
-                    this.popup.classList.add(this.className);
-                    this[addEventListenersToCloseBtn]();
+                    if (this.btnShow === this.btnClose) {
+                        this.popup.classList.toggle(this.className);
+                    } else {
+                        this.addClasslist();
+                    }
                     
-                    document.body.classList.add('no-scroll');
-                    window.addEventListener("touchmove", PopUp.preventScrollOnMobile);
+                    if (this.noScroll) {
+                        PopUp.addNoScroll();
+                    }
                 });
             })
         }
+    };
+    
+    /**
+     * Prevent body scroll
+     */
+    static addNoScroll() {
+        document.body.classList.add('no-scroll');
+        window.addEventListener("touchmove", PopUp.preventScrollOnMobile);
+    };
+    
+    static removeNoScroll() {
+        document.body.classList.remove('no-scroll');
+        window.removeEventListener("touchmove", PopUp.preventScrollOnMobile);
     };
     
     /**
@@ -61,15 +84,20 @@ const PopUp = class {
         this.popup.classList.remove(this.className);
         this[removeEventListenersToCloseBtn]();
         
-        document.body.classList.remove('no-scroll');
-        window.removeEventListener("touchmove", PopUp.preventScrollOnMobile);
+        if (this.noScroll) {
+            PopUp.removeNoScroll();
+        }
     };
+    
+    addClasslist() {
+        this.popup.classList.add(this.className);
+        this[addEventListenersToCloseBtn]();
+    }
     
     [addEventListenersToCloseBtn]() {
         const {btnClose} = this;
         
         if (!Array.isArray(btnClose)) {
-            
             btnClose.addEventListener(this.actionHide, ({target}) => (target === btnClose) ? this[hidePopUp](target) : null);
         } else {
             btnClose.forEach(btn => {
