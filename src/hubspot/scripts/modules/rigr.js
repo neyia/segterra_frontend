3d715860e28a61214f583e74945f440d9b963e17
EkaterinaKaliago <kaliago@yandex.ru>
2019-07-25 15:01:54 +0300

    RIGR-304 Rigr Hubspot Landing Page ready

--- src/hubspot/scripts/modules/rigr.js
+++ src/hubspot/scripts/modules/rigr.js
@@ -1,6 +1,6 @@
 /** Deconste ALL imports before adding to HubSpot*/
 
-import PopUp from './PopUp'
+//import PopUp from './PopUp'
 
 window.addEventListener('load', () => {
     mobileMenu();
@@ -60,5 +60,37 @@ function showPopup() {
 //     IputEffect();
 // });
 
+// "use strict";
+//
+// window.addEventListener('load', function () {
+//     showPopup();
+// });
+//
+// function showPopup() {
+//     var faq = document.querySelector('.faq-popup');
+//     var policy = document.querySelector('.policy-popup');
+//     var terms = document.querySelector('.terms-popup');
+//
+//     var faqBtn = document.querySelectorAll('.faq');
+//     var policyBtn = document.querySelectorAll('.policy');
+//     var termsBtn = document.querySelectorAll('.terms');
+//
+//     var closeBtn = document.querySelectorAll('.close-btn');
+//     var closeMobile = document.querySelectorAll('.close-mobile');
+//
+//     faqBtn = Array.prototype.slice.call( faqBtn, 0 );
+//     policyBtn = Array.prototype.slice.call( policyBtn, 0 );
+//     termsBtn = Array.prototype.slice.call( termsBtn, 0 );
+//
+//     closeBtn = Array.prototype.slice.call( closeBtn, 0 );
+//     closeMobile = Array.prototype.slice.call( closeMobile, 0 );
+//
+//
+//
+//     new PopUp(faqBtn, faq, [].concat(closeBtn, closeMobile), true);
+//     new PopUp(policyBtn, policy, [].concat(closeBtn, closeMobile), true);
+//     new PopUp(termsBtn, terms, [].concat(closeBtn, closeMobile), true);
+// }
+//
 
 
