7999f83c57a3e10b4f7270f557a4b74be3b48211
pamagiti <xiamkaix@gmail.com>
2021-06-17 00:52:25 +0300

    podcast page - card (v1)

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ const path = {
     src: {
         html: 'src/*.html',
         js: [ 'src/scripts/app.js', 'src/hubspot/scripts/hubspot.js', 'src/hubspot/scripts/hubspotRigr.js', 'src/rigr_blog/scripts/hubspotRigrBlog.js'],
-        style: [ 'src/styles/style.less', 'src/rigr_blog/styles/rigrBlogStyles.less', 'src/styles/immunity_plans.less', 'src/styles/hubspot_template.less', 'src/styles/podcast/podcast.less'],
+        style: [ 'src/styles/style.less', 'src/rigr_blog/styles/rigrBlogStyles.less', 'src/styles/immunity_plans.less', 'src/styles/hubspot_template.less', 'src/styles/hubspot_email_pages.less', 'src/styles/podcast/podcast.less'],
         img: ['src/assets/img/**/*.*','src/hubspot/assets/img/**/*.*', 'src/rigr_blog/assets/img/**/*.*'],
         image: 'src/image/**/*.*',
         svg: ['src/assets/svg/*.*', 'src/hubspot/assets/svg/*.*'],
@@ -93,7 +93,7 @@ gulp.task('styles', () => {
 });
 
 gulp.task('scripts', () => {
-    
+
     return gulp.src(path.src.js)
         .pipe(webpack( require('./webpack.config.js') ))
         .pipe(gulp.dest(path.build.js))
