38b8eb841a66ad94811677ba622c18527dffc003
kate <kaliago@yandex.ru>
2021-06-24 16:29:25 +0300

    QS-12549 podcast page hubspot implementation

--- src/scripts/podcast.js
+++ src/scripts/podcast.js
@@ -1,23 +1,34 @@
 window.addEventListener('load', () => {
-
+    
     const popupLink = document.getElementById("card__popup-link");
     const popup= document.getElementById('popup');
     const close = document.getElementById('close');
     const screen = document.getElementById('screen');
-
-    const podcastForm = document.getElementById('podcast-form');
-
-    podcastForm.onsubmit = function(e) {
-        const success = document.querySelector('.top-section__success');
+    
+    // const podcastForm = document.getElementById('podcast-form');
+    //
+    // podcastForm.onsubmit = function(e) {
+    //     const success = document.querySelector('.top-section__success');
+    //     e.preventDefault();
+    //     this.classList.add("hide");
+    //     success.classList.add("fadeIn");
+    //     const fadeOut = function () {
+    //         success.classList.add("fadeOut")
+    //     }
+    //     setTimeout(fadeOut,5000);
+    // };
+    
+    // for hubspot
+    const podcastForm = document.getElementsByClassName('podcast-form-wrapper-js')[0];
+    podcastForm.addEventListener('submit', function(e){
         e.preventDefault();
-        this.classList.add("hide");
-        success.classList.add("fadeIn");
         const fadeOut = function () {
+            const success = document.getElementsByClassName('submitted-message')[0];
             success.classList.add("fadeOut")
         }
         setTimeout(fadeOut,5000);
-    };
-
+    });
+    
     popupLink.onclick = function () {
         openPopup();
     };
